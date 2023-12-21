e7f729970374fd3e293f70c1f8b21155671f8f3e
rifqipr <m.rifqi.p80@gmail.com>
2023-12-21 17:10:11 +0700

    added machine learning

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from app.controllers import art_controller, user_controller
+from app.controllers import art_controller, user_controller, predict_controller
 from app.services.database import init_db
 
 app = FastAPI()
-
 
 init_db()
 
 app.include_router(art_controller.router)
 app.include_router(user_controller.router)
+app.include_router(predict_controller.router)
